48c6da8ba3ca5c2024d7d6368eb265394ff74d9d
michael <amarmic@post.bgu.ac.il>
2021-04-15 10:39:27 +0300

    add acceptance tests

--- dev/test/acceptence/Login.test.ts
+++ dev/test/acceptence/Login.test.ts
@@ -15,8 +15,26 @@ describe('login system test' , function() {
         }
     })
 
-    it('l' , function() {
+    it('user false password login' , function() {
+        let service: Service = Service.get_instance();
+        service.clear();
+        let res:Result<number> = service.enter();
+        if (isOk(res))
+        {
+            service.register("avi", "123456789");
+            expect(isOk(service.login(res.value,"avi","1234"))).to.equal(false);
+        }
+    })
 
+    it('user false username login' , function() {
+        let service: Service = Service.get_instance();
+        service.clear();
+        let res:Result<number> = service.enter();
+        if (isOk(res))
+        {
+            service.register("avi", "123456789");
+            expect(isOk(service.login(res.value,"yogev ha'melech","123456789"))).to.equal(false);
+        }
     })
 
 
--- dev/test/user/register.test.ts
+++ dev/test/user/register.test.ts
@@ -4,13 +4,15 @@ import { Authentication } from '../../src/DomainLayer/user/Authentication';
 import { isOk } from '../../src/Result';
 
 describe('register tests' , function() {
-    Authentication.clean();
+    
     
     it('good register', function(){
+        Authentication.clean();
         expect(isOk(Register.register("avi","123456"))).to.equal(true);
     });
 
     it('register with used user name', function(){
+        Authentication.clean();
         Register.register("avi","123456");
         expect(isOk(Register.register("avi","11123"))).to.equal(false);
     });
