0d36fcec2650aa29aa51438c28f72fc7c6726c2b
AlonMaor14 <maoralo@post.bgu.ac.il>
2021-04-17 21:54:33 +0300

    more tests

--- dev/test/store/inventory.test.ts
+++ dev/test/store/inventory.test.ts
@@ -1,4 +1,4 @@
-import { expect } from "chai"
+import { expect, assert } from "chai"
 import { Category } from "../../src/DomainLayer/store/Common"
 import { Inventory } from "../../src/DomainLayer/store/Inventory"
 import { ProductDB } from "../../src/DomainLayer/store/ProductDB"
@@ -9,19 +9,27 @@ describe('reserve product' , () => {
     it('test reserve product', () => {
 
         let inventory = new Inventory()
-        inventory.addNewProduct('somthing', [Category.COMPUTER], 1, 30, 30)
-        let productId = ProductDB.getProductByName('somthing').getProductId()
-        expect(isOk(inventory.reserveProduct(productId, 10))).to.equal(true)
-        expect(inventory.isProductAvailable(productId, 19)).to.equal(true)
-        expect(inventory.isProductAvailable(productId, 30)).to.equal(false)
+        let productId = inventory.addNewProduct('somthing', [Category.COMPUTER], 1, 30, 30)
+        if(isOk(productId)){
+            expect(isOk(inventory.reserveProduct(productId.value, 10))).to.equal(true)
+            expect(inventory.isProductAvailable(productId.value, 19)).to.equal(true)
+            expect(inventory.isProductAvailable(productId.value, 30)).to.equal(false)
+        }
+        else {
+            assert.fail('failed add new product')
+        }
     })
 
     it('test return reserve product', () => {
         let inventory = new Inventory()
-        inventory.addNewProduct('somthing', [Category.COMPUTER], 1, 30)
-        let productId = ProductDB.getProductByName('somthing').getProductId()
-        inventory.returnReservedProduct(productId, 10)
-        expect(inventory.isProductAvailable(productId, 10)).to.equal(true)
-        expect(inventory.isProductAvailable(productId, 30)).to.equal(false)
+        let productId = inventory.addNewProduct('somthing', [Category.COMPUTER], 1, 30)
+        if(isOk(productId)){
+            inventory.returnReservedProduct(productId.value, 10)
+            expect(inventory.isProductAvailable(productId.value, 10)).to.equal(true)
+            expect(inventory.isProductAvailable(productId.value, 30)).to.equal(false)
+        }
+        else {
+            assert.fail('failed add new product')
+        }
     })
 })
