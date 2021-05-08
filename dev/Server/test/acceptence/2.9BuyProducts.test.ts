b2b61b93b19fd7996c8d199fd8651e9c271bc126
Elad Feldman <eladfeld@post.bgu.ac.il>
2021-05-08 16:17:29 +0300

    temp

--- dev/Server/test/acceptence/2.9BuyProducts.test.ts
+++ dev/Server/test/acceptence/2.9BuyProducts.test.ts
@@ -11,7 +11,6 @@ import { isFailure, isOk, Result } from '../../src/Result';
 import { SystemFacade } from '../../src/DomainLayer/SystemFacade'
 import { Service } from '../../src/ServiceLayer/Service';
 import { register_login, open_store } from './common';
-
 describe('2.9: buy products', function () {
 
     var service: Service = Service.get_instance();
@@ -51,5 +50,35 @@ describe('2.9: buy products', function () {
         service.checkoutBasket(sessionId, store.getStoreId(), "king Goerge st 42")
         .then(_ => assert.fail)
         .catch(_ => assert.ok)
+    })
+
+    it('parallel buy of last item',async function () {
+        let avi_sessionId = await service.enter()
+        let ali_sessionId = await service.enter()
+
+        let avi =await register_login(service,avi_sessionId, "avi", "1234");
+        let ali =await register_login(service,ali_sessionId, "ali", "1234");
+
+        let store = await open_store(service,avi_sessionId, avi, "Mega", 123456, "Tel aviv");
+        store.addCategoryToRoot('Sweet')
+        let banana =await service.addNewProduct(avi_sessionId, store.getStoreId(), "banana", ['Sweet'], 1, 1);
+
+
+        await service.addProductTocart(avi_sessionId, store.getStoreId(), banana, 40);
+        await service.addProductTocart(ali_sessionId, store.getStoreId(), banana, 40);
+
+
+        // the following service calls are asycn and wont block
+        service.checkoutBasket(avi_sessionId, store.getStoreId(), "king Goerge st 42");
+        let avi_buy_res = service.completeOrder(avi_sessionId, store.getStoreId(), new PaymentInfo(1234, 456, 2101569), "user address");
+
+        service.checkoutBasket(ali_sessionId, store.getStoreId(), "king Goerge st 42");
+        let ali_buy_res = service.completeOrder(ali_sessionId, store.getStoreId(), new PaymentInfo(1234, 456, 2101569), "user address");
+
+        avi_buy_res.then( avi_buy =>{
+            ali_buy_res.then( ali_buy => {
+                assert(((avi_buy === true) && (ali_buy === false)) || ((avi_buy === false) && (ali_buy === true)))
+            })
+        })
     })
 });
