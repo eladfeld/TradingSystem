79ceb59f743bc3fb8fb69a33d9c41af88a4da9c1
Tzuri Teshuba <teshuba@post.bgu.ac.il>
2021-06-16 23:07:30 +0300

    load and stress tests and report

--- dev/Server/test/stress/csv_maker.py
+++ dev/Server/test/stress/csv_maker.py
@@ -1,15 +1,17 @@
-USER_FILE = "users.csv"
-NUM_USERS = 100
+SUBSCRIBERS_FILE = "users.csv"
+NUM_SUBSCRIBERS = 5000000
 
-def prepare_users():
-    file = open(USER_FILE, "w")
-    for i in range(0, NUM_USERS):
-        file.write(f'user_stress_{i},123,25,store_stress_{i},product_stress_{i}\n')
+def prepare_subscribers():
+    file = open(SUBSCRIBERS_FILE, "w")
+    for i in range(0, NUM_SUBSCRIBERS):
+        #username, password, age, storeId, productId,quantity, checkoutInfo,
+        file.write(f'sub_stress_{i}\n',)
     file.close()
 
+def prepare_files():
+    prepare_subscribers()
 
-
-prepare_users()
+prepare_files()
 
 
 
